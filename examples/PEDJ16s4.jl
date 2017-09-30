330c0e17846e9fde56889e67b92d11225dc24460
Benoît Legat <benoit.legat@gmail.com>
2017-09-30 18:08:59 +0200

    Add PEDJ example

---
+++ examples/PEDJ16s4.jl
@@ -0,0 +1,22 @@
+# Section 4 of
+# M. Philippe, R. Essick, G. E. Dullerud and R. M. Jungers.
+# "Stability of discrete-time switching systems with constrained switching sequences."
+# Automatica, 72:242-250, 2016
+# The JSR is 0.9748171979372074
+
+A = [0.94 0.56; 0.14 0.46]
+B = [0; 1]
+k1 = -0.49
+k2 = 0.27
+As = [A + B * [k1 k2], A + B * [0 k2], A + B * [k1 0], A]
+G = LightAutomaton(4)
+add_transition!(G, 1, 2, 3)
+add_transition!(G, 2, 1, 2)
+add_transition!(G, 1, 3, 1)
+add_transition!(G, 3, 1, 2)
+add_transition!(G, 2, 3, 1)
+add_transition!(G, 3, 2, 3)
+add_transition!(G, 3, 3, 1)
+add_transition!(G, 3, 4, 4)
+add_transition!(G, 4, 3, 1)
+hs = discreteswitchedsystem(As, G)
