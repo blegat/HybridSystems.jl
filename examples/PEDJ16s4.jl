559dddd81d46d561b136a455a6b295e99459676f
Benoît Legat <benoit.legat@gmail.com>
2017-10-01 23:44:35 +0200

    Use SMatrix for PEDJ

--- examples/PEDJ16s4.jl
+++ examples/PEDJ16s4.jl
@@ -1,14 +1,22 @@
 # Section 4 of
-# M. Philippe, R. Essick, G. E. Dullerud and R. M. Jungers.
-# "Stability of discrete-time switching systems with constrained switching sequences."
+# [PEDJ16] M. Philippe, R. Essick, G. E. Dullerud and R. M. Jungers.
+# Stability of discrete-time switching systems with constrained switching sequences.
 # Automatica, 72:242-250, 2016
+# and running example of
+# [LJP16] Legat, B.; Jungers, R. M. & Parrilo, P. A.
+# Generating unstable trajectories for Switched Systems via Dual Sum-Of-Squares techniques
+# Proceedings of the 19th International Conference on Hybrid Systems: Computation and Control, ACM, 2016, 51-60
 # The JSR is 0.9748171979372074
 
-A = [0.94 0.56; 0.14 0.46]
+using HybridSystems
+using StaticArrays
+
+A = @SMatrix [0.94 0.56; 0.14 0.46]
 B = [0; 1]
 k1 = -0.49
 k2 = 0.27
 As = [A + B * [k1 k2], A + B * [0 k2], A + B * [k1 0], A]
+@assert eltype(As) <: SMatrix
 G = LightAutomaton(4)
 add_transition!(G, 1, 2, 3)
 add_transition!(G, 2, 1, 2)
