e829c46f3d749b0017c0d98869cc3fa626ff48a7
Benoît Legat <benoit.legat@gmail.com>
2018-01-13 10:30:41 +0100

    Drop CDDLib dependency in tests

--- examples/horizontal_jump.jl
+++ examples/horizontal_jump.jl
@@ -1,20 +1,19 @@
 using Systems
 using HybridSystems
 using Polyhedra
-using CDDLib
 
-function horizontal_jump_example(shift::Bool=false)
+function horizontal_jump_example(lib::PolyhedraLibrary, shift::Bool=false)
     A = LightAutomaton(2)
     add_transition!(A, 1, 2, 1)
     add_transition!(A, 2, 1, 1)
 
     if shift
-        H1 = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [ones(2); ones(2)]), CDDLibrary())
+        H1 = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [ones(2); ones(2)]), lib)
     else
-        H1 = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [1, 3, 1, -1]), CDDLibrary())
+        H1 = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [1, 3, 1, -1]), lib)
     end
-    H2 = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [4, 1 + shift, -2, 1]), CDDLibrary())
-    U = polyhedron(SimpleHRepresentation(reshape([1., -1.], 2, 1), [2., -2.]), CDDLibrary())
+    H2 = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [4, 1 + shift, -2, 1]), lib)
+    U = polyhedron(SimpleHRepresentation(reshape([1., -1.], 2, 1), [2., -2.]), lib)
 
     S = ConstrainedDiscreteIdentitySystem.(2, [H1, H2])
 
