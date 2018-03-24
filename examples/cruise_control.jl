ea78561a0944f83818002d33fe19fcfad74132be
Benoît Legat <benoit.legat@gmail.com>
2018-03-24 01:59:43 +0100

    Add cruise control tests

--- examples/cruise_control.jl
+++ examples/cruise_control.jl
@@ -9,6 +9,13 @@ using HybridSystems
 using Polyhedra
 using CDDLib
 
+"""
+    cruise_control_example(N, M; vmin = 5., vmax = 35., v = (15.6, 24.5), U = 4, D = 0.5, ks = 4500, kd = 4600, m = 1000, m0 = 100, H = 0.8, T = 2, h = H / T, sym = false)
+
+Hybrid System representing the speed of a truck and its `M` trailers.
+The mass of the truck is `m0` and the mass of each trailer is `m`.
+The dynamic is discretized over step sizes of length `h`.
+"""
 function cruise_control_example(N, M; vmin = 5., vmax = 35., v = (15.6, 24.5), U = 4, D = 0.5, ks = 4500, kd = 4600, m = 1000, m0 = 100, H = 0.8, T = 2, h = H / T, sym = false)
     function Pv(v, maxspeed)
         s = maxspeed ? 1. : -1.
@@ -48,10 +55,10 @@ function cruise_control_example(N, M; vmin = 5., vmax = 35., v = (15.6, 24.5), U
     elseif N == 2
         add_transition!(G, 1, 2, 1)
         add_transition!(G, 2, 2, 1)
-        I = [P1 ∩ Pv0max, P1 ∩ Pvimax[1]]
+        safe_sets = [P1 ∩ Pv0max, P1 ∩ Pvimax[1]]
     else
         @assert N == 1 + (T+1) * length(v)
-        I = Vector{typeof(P1)}(N)
+        safe_sets = Vector{typeof(P1)}(N)
         function new_speed_signal!(u)
             for (j, vj) in enumerate(v)
                 w = (T+1)*(j-1) + T+1
@@ -63,21 +70,20 @@ function cruise_control_example(N, M; vmin = 5., vmax = 35., v = (15.6, 24.5), U
                 u = (T+1)*(i-1) + t
                 if t == 1
                     add_transition!(G, u, u, 1)
-                    I[u] = P1 ∩ Pvimax[i]
+                    safe_sets[u] = P1 ∩ Pvimax[i]
                 else
                     add_transition!(G, u, u-1, 1)
-                    I[u] = P1 ∩ Pv0max
+                    safe_sets[u] = P1 ∩ Pv0max
                 end
                 new_speed_signal!(u)
             end
         end
         add_transition!(G, N, N, 1)
-        I[N] = P1 ∩ Pv0max
+        safe_sets[N] = P1 ∩ Pv0max
         new_speed_signal!(N)
     end
 
     d = 2M + 2
-    is = DiscreteIdentitySystem(d)
     #s = DiscreteLinearControlSystem([1. 0; 0 1], reshape([1.; 0], 2, 1), U)
     A = zeros(d-1, d-1)
     for i in 1:M
@@ -103,21 +109,18 @@ function cruise_control_example(N, M; vmin = 5., vmax = 35., v = (15.6, 24.5), U
     A1 = [eye(d-1)+h*A [zeros(d-2); h]
           zeros(d-1)'  0]
     B = reshape([zeros(d-1); 1.], d, 1)
-    s1 = DiscreteLinearControlSystem(A1, B)
+    s1 = LinearControlDiscreteSystem(A1, B)
     A2 = [eye(d-1)     zeros(d-1)
           zeros(d-1)'  0]
-    s2 = DiscreteLinearControlSystem(A2, B)
+    s2 = LinearControlDiscreteSystem(A2, B)
 
     sw = AutonomousSwitching()
 
-    fs = FullSpace()
-
     M = LightGraphs.ne(G.G)
 
-    S = Fill(is, N)
-    Gu = Fill(fs, M)
+    S = ConstrainedDiscreteIdentitySystem.(d, safe_sets)
     Re = [s1, s2]
     Sw = Fill(sw, N)
 
-    HybridSystem(G, S, I, Gu, Re, Sw)
+    HybridSystem(G, S, Re, Sw)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,62 +3,73 @@ using Base.Test
 using Polyhedra
 using FillArrays
 
-@testset "Horizontal jump" begin
-    include("../examples/horizontal_jump.jl")
-    for shift in (false, true)
-        s = horizontal_jump_example(SimplePolyhedraLibrary{Float64}(), shift)
+@testset "Switched System" begin
+    square = convexhull([1, 1], [-1, 1], [-1, -1], [1, -1])
+    simplex = convexhull([0, 0], [0, 1], [1, 0])
+
+    @testset "Switched System" begin
+        s = discreteswitchedsystem([ones(2, 2), zeros(2, 2)])
+        @test nstates(s) == 1
+        @test states(s) == Base.OneTo(1)
+        @test statedim(s, 1) == 2
+        @test ntransitions(s) == 2
+        @test eltype(transitions(s)) == transitiontype(s)
+        @test transitions(s) == HybridSystems.OneStateTransition.([1, 2])
+        @test source.(s, transitions(s)) == [1, 1]
+        @test event.(s, transitions(s)) == [1, 2]
+        @test target.(s, transitions(s)) == [1, 1]
+        @test length(transitions(s)) == 2
+        @test in_transitions(s, 1) == HybridSystems.OneStateTransition.([1, 2])
+        @test out_transitions(s, 1) == HybridSystems.OneStateTransition.([1, 2])
+    end
+
+    @testset "State Dependent Switched System" begin
+        s = discreteswitchedsystem([ones(2, 2), zeros(2, 2)], [square, simplex])
         @test nstates(s) == 2
         @test states(s) == 1:2
-        for u in states(s)
-            @test statedim(s, u) == 2
-            @test stateset(s, u) isa Polyhedra.SimplePolyhedron{2,Float64}
-        end
-        @test length(transitions(s)) == 2
-        @test ntransitions(s) == 2
-        for t in transitions(s)
-            @test stateset(s, t) isa Polyhedra.SimplePolyhedron{2,Float64}
-            @test inputset(s, t) isa Polyhedra.SimplePolyhedron{1,Float64}
-        end
+        @test all(statedim.(s, states(s)) .== 2)
+        @test stateset(s, 1) === square
+        @test stateset(s, 2) === simplex
+        @test length(transitions(s)) == 4
+        @test ntransitions(s) == 4
+    end
+
+    @testset "State Dependent Switched System" begin
+        s = discreteswitchedsystem([ones(2, 2), zeros(2, 2)], Fill(square, 2))
+        @test nstates(s) == 2
+        @test states(s) == 1:2
+        @test all(statedim.(s, states(s)) .== 2)
+        @test stateset(s, 1) === square
+        @test stateset(s, 2) === square
+        @test length(transitions(s)) == 4
+        @test ntransitions(s) == 4
     end
 end
 
-square = convexhull([1, 1], [-1, 1], [-1, -1], [1, -1])
-simplex = convexhull([0, 0], [0, 1], [1, 0])
-
-@testset "Switched System" begin
-    s = discreteswitchedsystem([ones(2, 2), zeros(2, 2)])
-    @test nstates(s) == 1
-    @test states(s) == Base.OneTo(1)
-    @test statedim(s, 1) == 2
-    @test ntransitions(s) == 2
-    @test eltype(transitions(s)) == transitiontype(s)
-    @test transitions(s) == HybridSystems.OneStateTransition.([1, 2])
-    @test source.(s, transitions(s)) == [1, 1]
-    @test event.(s, transitions(s)) == [1, 2]
-    @test target.(s, transitions(s)) == [1, 1]
-    @test length(transitions(s)) == 2
-    @test in_transitions(s, 1) == HybridSystems.OneStateTransition.([1, 2])
-    @test out_transitions(s, 1) == HybridSystems.OneStateTransition.([1, 2])
-end
-
-@testset "State Dependent Switched System" begin
-    s = discreteswitchedsystem([ones(2, 2), zeros(2, 2)], [square, simplex])
-    @test nstates(s) == 2
-    @test states(s) == 1:2
-    @test all(statedim.(s, states(s)) .== 2)
-    @test stateset(s, 1) === square
-    @test stateset(s, 2) === simplex
-    @test length(transitions(s)) == 4
-    @test ntransitions(s) == 4
-end
-
-@testset "State Dependent Switched System" begin
-    s = discreteswitchedsystem([ones(2, 2), zeros(2, 2)], Fill(square, 2))
-    @test nstates(s) == 2
-    @test states(s) == 1:2
-    @test all(statedim.(s, states(s)) .== 2)
-    @test stateset(s, 1) === square
-    @test stateset(s, 2) === square
-    @test length(transitions(s)) == 4
-    @test ntransitions(s) == 4
+@testset "Examples" begin
+    @testset "Horizontal jump" begin
+        include("../examples/horizontal_jump.jl")
+        for shift in (false, true)
+            s = horizontal_jump_example(SimplePolyhedraLibrary{Float64}(), shift)
+            @test nstates(s) == 2
+            @test states(s) == 1:2
+            for u in states(s)
+                @test statedim(s, u) == 2
+                @test stateset(s, u) isa Polyhedra.SimplePolyhedron{2,Float64}
+            end
+            @test length(transitions(s)) == 2
+            @test ntransitions(s) == 2
+            for t in transitions(s)
+                @test stateset(s, t) isa Polyhedra.SimplePolyhedron{2,Float64}
+                @test inputset(s, t) isa Polyhedra.SimplePolyhedron{1,Float64}
+            end
+        end
+    end
+    @testset "Cruise Control" begin
+        include("../examples/cruise_control.jl")
+        for sym in (false, true)
+            s = cruise_control_example(7, 1, sym=sym)
+            @test nstates(s) == 7
+        end
+    end
 end
