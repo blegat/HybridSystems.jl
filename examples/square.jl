121f18fab7bc514089f1c82e14e9a6af7ba5593f
Benoît Legat <benoit.legat@gmail.com>
2018-10-05 11:55:33 +0200

    Update square.jl and add notebook

--- examples/square.jl
+++ examples/square.jl
@@ -5,20 +5,18 @@ add_transition!(A, 1, 1, 1)
 
 using Polyhedra
 using CDDLib
-H = polyhedron(SimpleHRepresentation([eye(2); -eye(2)], [ones(2); ones(2)]), CDDLibrary())
+using LinearAlgebra
+hr = HalfSpace([1, 0], 1) ∩ HalfSpace([-1, 0], 1) ∩ HalfSpace([0, 1], 1) ∩ HalfSpace([0, -1], 1)
+H = polyhedron(hr, CDDLibrary())
 
-is = DiscreteIdentitySystem(2)
-s = DiscreteLinearControlSystem(eye(2), Matrix{Float64}(2, 0))
+using MathematicalSystems
+is = ConstrainedContinuousIdentitySystem(2, H)
+s = LinearControlDiscreteSystem(Matrix(1.0I, 2, 2), Matrix{Float64}(undef, 2, 0))
 
 sw = AutonomousSwitching()
 
-using SemialgebraicSets
-fs = FullSpace()
-
 S = Fill(is, 1)
-In = Fill(H, 1)
-Gu = Fill(fs, 1)
 Re = Fill(s, 1)
 Sw = Fill(sw, 1)
 
-hs = HybridSystem(A, S, In, Gu, Re, Sw)
+hs = HybridSystem(A, S, Re, Sw)
