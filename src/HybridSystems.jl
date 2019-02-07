e061fc8cd18721a4976885840e76bcd42636276a
mforets <mforets@gmail.com>
2019-02-07 10:35:09 -0300

    add guard function

--- src/HybridSystems.jl
+++ src/HybridSystems.jl
@@ -94,11 +94,15 @@ Returns the set of allowed states of the system at mode `u`.
 MathematicalSystems.stateset(hs::HybridSystem, u::Int) = stateset(hs.modes[u])
 
 """
-    stateset(s::AbstractSystem, t)
+    guard(hs::HybridSystem, t)
 
 Returns the guard for the transition `t`.
 """
-MathematicalSystems.stateset(hs::HybridSystem, t) = stateset(hs.resetmaps[symbol(hs, t)])
+guard(hs::HybridSystem, t) = stateset(hs.resetmaps[symbol(hs, t)])
+
+# for completeness, extend the stateset function from MathematicalSystems
+# because guards are given as the state constraints of the reset map
+MathematicalSystems.stateset(hs::HybridSystem, t) = guard(hs, t)
 
 """
     inputdim(s::AbstractSystem, u::Int)
